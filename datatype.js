1d138c5ca44f70e026377962798a6f7ed192645c
marietrull <marietrull@gmail.com>
2018-03-14 18:13:32 -0500

    Answered Datatype Questions

---
+++ datatype.js
@@ -0,0 +1,37 @@
+//QUESTIONS - PROVIDE DATATYPE + EX OF DATA STRUCTURE
+// A light switch that can be either on or off.
+	Data Type: Boolean
+	switchOn = true //meaning the switch is on
+	switchOff = false// meaing the switch is off
+// A user's email address.
+	Data Type: String
+	userEmail = 'MarieTrull@Gmail.com'
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+	Data Type: Array
+	spaceship: ['hull' , 'laser blasters', 'tractor beam', 'warp drive']
+// A list of student names from our class.
+	Data Type: Array
+	classNames: ['Marie' , 'Sam' , 'Sam Kazakhstan']
+// A list of student names from our class, each with a location.
+	Data Type: Object
+	const classList = {
+		Marie:
+		{ name: 'Marie',
+		  location: 'Chicago'},
+		Sam:
+		{ name: 'Sam',
+		  location: 'Kazakhstan'}
+	}
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+	Data Type: Object
+	const classList2 = {
+		Marie:
+		{	name: 'Marie',
+			location: 'Chicago',
+			tvshows: ['GoT', 'Westworld', 'Seinfeld', 'BillMaher']  },
+		Sam:
+		{ 	name: 'Sam',
+		  	location: 'Kazakhstan',
+		  	tvshows: ['Pretty Little Liars', 'The Bachelor', 'WWE'] },
+		}
+	}
