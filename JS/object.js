f0149b30a2d8f65aa4d2df688b20dfac0eb7d3e6
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-03-15 04:38:15 -0500

    accesed limbo

--- JS/object.js
+++ JS/object.js
@@ -92,6 +92,28 @@ myself.hobby;
 myself['hobby'];
 
 
+//Object-ception
+
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+
 
 
 
