893e7f6e29640730c1ab01517f84b131d2989503
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-03-15 04:21:17 -0500

    selecting datatypes done

---
+++ JS/object.js
@@ -0,0 +1,65 @@
+/*
+
+1.Boolean
+
+
+const light = {
+	type: 'light swith',
+	On: false
+}
+
+2. string
+
+const person = {
+	name:'John',
+	email: 'example@email.com'
+}
+
+
+3.array
+
+
+const vehicle = {
+	type: "space ship",
+	features: ['heull', 'laser blasters', 'tractor beam', 'warp drive']
+
+}
+
+
+4.array
+
+const class = {
+	class: 'General A'
+	type: 'WDI',
+	students:['MJ','Sam', 'Tyler', 'Zoe']
+}
+
+5.object
+
+
+const class = {
+	class: 'General A'
+	type: 'WDI',
+	students:[{name: 'MJ', address: '123 Wacker'},{name:'Zoe', address: '123 drive'},{name:'Tyler', address:'123 example ST'}]
+}
+
+
+
+6.array
+
+
+
+const class = {
+	class: 'General A'
+	type: 'WDI',
+	students:[{name: 'MJ', address: '123 Wacker',shows:['seinfeld', 'grays anatomy','Shark Tank']},{name:'Zoe', address: '123 drive',shows:['seinfeld', 'grays anatomy','Shark Tank']},{name:'Tyler', address:'123 example ST',shows:['seinfeld', 'grays anatomy','Shark Tank']}]
+}
+
+
+
+
+
+
+
+
+*/
