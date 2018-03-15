aa2739391ed0d09bfcf979bcbb51dea1ab607150
agraham <agraham0925@gmail.com>
2018-03-14 23:52:32 -0500

    accessed limbo

--- JS_datatypes.js
+++ JS_datatypes.js
@@ -76,3 +76,13 @@ const alyssaGraham = {
 
 console.log(Alyssa.hobby);
 
+
+//OBJECT-CEPTION
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo)
+
+
+
+
