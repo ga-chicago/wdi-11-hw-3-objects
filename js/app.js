510fcb120c3d0f838ff8c29ab1fadc18b7a09b28
Samat Baltin <somych1@gmail.com>
2018-03-14 22:18:44 -0500

    selecting datatypes done

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Answer the Following
+
+// 1. string
+let lightSwitch = 'on';
+lightSwitch = 'off';
+
+// 2. string
+const email = 'something@gmail.com'
+
+// 3. array
+const spaceship = ['hull', 'laser blaster', 'tractor beam', 'warp drive'];
+
+// 4. array with an objects
+const ourClass = [
+	{ name: 'student1', location: 'new york', favTv: 'family guy' },
+	{ name: 'student2', location: 'LA', favTv: 'news' }
+]
