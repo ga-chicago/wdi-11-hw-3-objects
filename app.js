3db8b78a0df5bd1420951dd41ce68d427a8e3c08
Hannah Werman <hannah.werman@gmail.com>
2018-03-14 21:31:35 -0500

    selecting datatypes done

---
+++ app.js
@@ -0,0 +1,47 @@
+// Datatypes
+
+// #1 - boolean. 
+lightOn = true 
+lightOn = false
+
+// #2 - string. 
+"me@example.com"
+
+// #3 - object 
+const spaceship = {
+	hull: metal;
+	laserBlasters: true;
+	tractorBeam: true;
+	warpDrive: true;
+}
+
+// #4 - array
+const students = ["Hannah", "Marie", "Alyssa"]
+
+// #5 - array of objects
+const class = [{
+	name: "Hannah",
+	location: "First Row"
+}, {
+	name: "Irwin", 
+	location: "Second Row"
+}, {
+	name: "Zoe",
+	location: "Third Row"
+}]
+
+// #6 - array of objects with an array of favorite shows
+const class = [{
+	name: "Hannah",
+	location: "First Row",
+	favoriteShows: ["How I Met Your Mother", "New Girl", "Agents of Shield"]
+}, {
+	name: "Irwin", 
+	location: "Second Row"
+	favoriteShows: ["Arrow", "This is Us", "WestWorld"]
+}, {
+	name: "Zoe",
+	location: "Third Row"
+	favoriteShows: ["WestWorld", "The Big Bang Theory", "Star Trek"]
+}]
+
