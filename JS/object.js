d289e88fda38c4d3534ced505bdb4a28e1230610
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-03-15 04:57:01 -0500

    made an arrey of odd bonds

--- JS/object.js
+++ JS/object.js
@@ -145,6 +145,7 @@ const bondFilms = [
   { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
 ];
 
+//1.
 
 const bondTitles = [];
 
@@ -153,6 +154,23 @@ for(let i = 0; i < bondFilms.length; i ++){
 }
 
 console.log(bondTitles);
+
+
+//2.
+
+const oddBonds = [];
+
+for (let i = 0; i < bondFilms.length; i ++){
+	if(bondFilms[i].year % 2 !== 0){
+		oddBonds.push(bondFilms[i].title);
+	}
+}
+
+
+console.log(oddBonds);
+
+
+
 
 
 
