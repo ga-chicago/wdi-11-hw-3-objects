b032960d7a95b246829b83959ea360c962c8d0ce
Zoe <zoereagan@gmail.com>
2018-03-14 20:52:36 -0500

    selecting data types done.

---
+++ homework.js
@@ -0,0 +1,34 @@
+// //03_14_18 homework
+// A light switch that can be either on or off.
+//boolean
+const roomDark = true;
+
+if (rooomDark = true) {
+  console.log ("Turn the light on!")
+
+// A user's email address.
+//string
+const userEmail = "zoereagan@gmail.com"
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+//an array with strings
+const spaceshipParts = ["hull", "laser blasters", "tractor beam", "warp drive"]
+
+// A list of student names from our class.
+//array with strings
+const studentNames = ["Hannah", "Ilyssa", "Marie", "Alexis", "Sam1", "Sam2", "Irwin", "Noemi", "MJ", "Tyler", "Ben"]
+
+// A list of student names from our class, each with a location.
+// anonymous object
+const studentList = [{
+    name: Zoe
+    location: Evanston
+  }]
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+//named objects with an object and an array
+const studentList = {
+  Zoe: {
+    location: "Evanston",
+    tv shows: ["Deadwood", "High Maintainence", "Planet Earth"]},
+}
